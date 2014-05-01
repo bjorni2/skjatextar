f7e0246bf38daf6c416f4d9fbef51a2ed272d1b7
johannbjarka <johannbjarka@gmail.com>
2014-05-01 10:02:25 +0000

    Test

--- SkjaTextar/Controllers/HomeController.cs
+++ SkjaTextar/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SkjaTextar.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "WAZZZZUP";
 
             return View();
         }
