ad28482fba2d1b557543701e85e73cd1074b7222
bjorni2 <bjornb13@ru.is>
2014-05-03 15:31:52 +0000

    Context, initializer og connection string

---
+++ SkjaTextar/DAL/SubTitleContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using SkjaTextar.Models;
+
+
+namespace SkjaTextar.DAL
+{
+    public class SubTitleContext : DbContext
+    {
+        public SubTitleContext() : base("SubTitleContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
