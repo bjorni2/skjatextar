6153e2e9f79d817af3cdb1b31cc523efadf5c140
ingolfurh <satan666>
2014-05-04 11:34:16 +0000

    Added Achievement class to Models folder

---
+++ SkjaTextar/Models/Achievement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkjaTextar.Models
+{
+	public class Achievement
+	{
+		public int ID { get; set; }
+		public string AchievementText { get; set; }
+	}
+}
