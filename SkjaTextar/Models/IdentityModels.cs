9da478d5b1926b11553189f76e208fe601f18663
bjorni2 <bjornb13@ru.is>
2014-05-04 17:08:28 +0000

    SSL virkjað

--- SkjaTextar/Models/IdentityModels.cs
+++ SkjaTextar/Models/IdentityModels.cs
@@ -10,7 +10,7 @@ namespace SkjaTextar.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("SubTitleContext")
         {
         }
     }
