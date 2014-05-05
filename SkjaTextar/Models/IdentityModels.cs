1089599a36bc878621ca8f1f2bbe2611fa5d69e3
bjorni2 <bjornb13@ru.is>
2014-05-05 10:40:25 +0000

    Skipti um dbContext, ApplicationUser -> User

--- SkjaTextar/Models/IdentityModels.cs
+++ SkjaTextar/Models/IdentityModels.cs
@@ -1,17 +1,35 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace SkjaTextar.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class User : IdentityUser
     {
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext()
             : base("SubTitleContext")
         {
+        }
+    }
+
+    public class SubTitleInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            // Example of test data
+
+            //var students = new List<Student>
+            //{
+            //new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+            //new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            //};
+
+            //students.ForEach(s => context.Students.Add(s));
+            //context.SaveChanges();
         }
     }
 }
