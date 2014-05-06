23228b666c5a328138538dbf0d8f49888e030680
bjorni2 <bjornb13@ru.is>
2014-05-06 10:54:41 +0000

    Meira seed

--- SkjaTextar/Models/IdentityModels.cs
+++ SkjaTextar/Models/IdentityModels.cs
@@ -31,6 +31,10 @@ namespace SkjaTextar.Models
         }
         public DbSet<Achievement> Achievements { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Show> Shows { get; set; }
+        public DbSet<Clip> Clips { get; set; }
+        public DbSet<Translation> Translations { get; set; }
     }
 
     public class ApplicationInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
@@ -67,6 +71,35 @@ namespace SkjaTextar.Models
                 new Movie{ ID = 2, CategoryID = 4, Title = "Lion King", ReleaseYear = 1995, Active = true },
                 new Movie{ ID = 3, CategoryID = 3, Title = "Brokeback mountain", ReleaseYear = 2005, Active = true },
             };
+            movies.ForEach(m => context.Movies.Add(m));
+            context.SaveChanges();
+
+            var shows = new List<Show>
+            {
+                new Show{ ID = 4, CategoryID = 4, Title = "Svampur Sveinsson", Series = 2, Episode = 13, ReleaseYear = 2009, Active = true },
+                new Show{ ID = 5, CategoryID = 4, Title = "Svampur Sveinsson", Series = 2, Episode = 14, ReleaseYear = 2009, Active = true },
+                new Show{ ID = 6, CategoryID = 3, Title = "House of cards", Series = 1, Episode = 3, ReleaseYear = 2013, Active = true },
+                new Show{ ID = 7, CategoryID = 1, Title = "How i met your mother", Series = 7, Episode = 13, ReleaseYear = 2011, Active = true },
+            };
+            shows.ForEach(s => context.Shows.Add(s));
+            context.SaveChanges();
+
+            var clips = new List<Clip>
+            {
+                new Clip{ ID = 8, CategoryID = 1, Title = "Nyan cat", ReleaseYear = 2009, Link = "www.youtube.com/watch?v=wZZ7oFKsKzY", Active = true },
+                new Clip{ ID = 9, CategoryID = 1, Title = "Charlie bit my finger", ReleaseYear = 2007, Link = "www.youtube.com/watch?v=_OBlgSz8sSM", Active = true }
+            };
+            clips.ForEach(c => context.Clips.Add(c));
+            context.SaveChanges();
+
+            var translations = new List<Translation>
+            {
+                new Translation{ ID = 1, Language = "Íslenska", MediaID = 9, NumberOfDownloads = 42, Score = 0, Locked = false },
+                new Translation{ ID = 2, Language = "Pólska", MediaID = 1, NumberOfDownloads = 3, Score = 1337, Locked = true },
+                new Translation{ ID = 3, Language = "Íslenska", MediaID = 1, NumberOfDownloads = 17, Score = 0, Locked = false },
+            };
+            translations.ForEach(t => context.Translations.Add(t));
+            context.SaveChanges();
         }
     }
 }
