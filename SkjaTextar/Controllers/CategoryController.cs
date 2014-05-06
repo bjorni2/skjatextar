c3318f9223c15cef759793bbf456ad59db378315
bjorni2 <bjornb13@ru.is>
2014-05-06 22:15:19 +0000

    Category Index

--- SkjaTextar/Controllers/CategoryController.cs
+++ SkjaTextar/Controllers/CategoryController.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace SkjaTextar.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController : BaseController
     {
         //
         // GET: /Category/
         public ActionResult Index()
         {
-            return View();
+            var model = _unitOfWork.CategoryRepository.Get().ToList();
+            return View(model);
+        }
+
+        public ActionResult CategoryIndex(int? id, string mediaType)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var model = _unitOfWork.MovieRepository.Get().Where(m => m.CategoryID == id).OrderBy(m => m.Title);
+            ViewBag.Category = _unitOfWork.CategoryRepository.GetByID(id).Name;
+            return View("IndexMovie", model);
         }
 	}
 }
