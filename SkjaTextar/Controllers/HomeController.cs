d9e421d06959d9281f8c6be58590295e074156ab
bjorni2 <bjornb13@ru.is>
2014-05-06 11:21:02 +0000

    Index/HomeController

--- SkjaTextar/Controllers/HomeController.cs
+++ SkjaTextar/Controllers/HomeController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SkjaTextar.Models;
+using SkjaTextar.ViewModels;
 
 namespace SkjaTextar.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
+        ApplicationDbContext _context = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var model = new HomeViewModel();
+            var topUser = _context.Users.ToList().OrderByDescending(u => u.Score).Take(5);
+            var topDownload = _context.Translations.OrderByDescending(t => t.NumberOfDownloads).Take(5);
+            var newTrans = _context.Translations.OrderByDescending(t => t.ID).Take(5);
+            model.ActiveUsers = topUser.ToList();
+            model.TopTranslations = topDownload.ToList();
+            model.NewTranslations = newTrans.ToList();
+            return View(model);
         }
 
         public ActionResult About()
