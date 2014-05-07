978f92e00017bbee7ab169891538a3e8f922f920
bjorni2 <bjornb13@ru.is>
2014-05-07 21:11:46 +0000

    Category Index

--- SkjaTextar/Controllers/CategoryController.cs
+++ SkjaTextar/Controllers/CategoryController.cs
@@ -24,22 +24,41 @@ namespace SkjaTextar.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ViewBag.Category = _unitOfWork.CategoryRepository.GetByID(id).Name;
+            if(id == 0)
+            {
+                ViewBag.Category = "Allt";
+            }
+            else
+            {
+                ViewBag.Category = _unitOfWork.CategoryRepository.GetByID(id).Name;
+            }
+
             switch (mediaType)
             {
                 case "Movie":
-                    var modelMovie = _unitOfWork.MovieRepository.Get().Where(m => m.CategoryID == id).OrderBy(m => m.Title);
-                    return View("MovieByCategory", modelMovie);
+                    var modelMovie = _unitOfWork.MovieRepository.Get();
+                    if(id == 0)
+                    {
+                        return View("MovieByCategory", modelMovie.OrderBy(m => m.Title));
+                    }
+                    return View("MovieByCategory", modelMovie.Where(m => m.CategoryID == id).OrderBy(m => m.Title));
                 case "Show":
-                    var modelShow = _unitOfWork.ShowRepository.Get().Where(m => m.CategoryID == id).OrderBy(m => m.Title);
-                    return View("ShowByCategory", modelShow);
+                    var modelShow = _unitOfWork.ShowRepository.Get();
+                    if(id == 0)
+                    {
+                        return View("ShowByCategory", modelShow.OrderBy(m => m.Title));
+                    }
+                    return View("ShowByCategory", modelShow.Where(m => m.CategoryID == id).OrderBy(m => m.Title));
                 case "Clip":
-                    var modelClip = _unitOfWork.ClipRepository.Get().Where(m => m.CategoryID == id).OrderBy(m => m.Title);
-                    return View("ClipByCategory", modelClip);
+                    var modelClip = _unitOfWork.ClipRepository.Get();
+                    if(id == 0)
+                    {
+                        return View("ClipByCategory", modelClip.OrderBy(m => m.Title));
+                    }
+                    return View("ClipByCategory", modelClip.Where(m => m.CategoryID == id).OrderBy(m => m.Title));
                 default:
                     return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            
         }
 	}
 }
