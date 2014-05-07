2a1f38dea7c6843cf860d6a81f581e5804e39ca4
bjorni2 <bjornb13@ru.is>
2014-05-07 14:28:56 +0000

    Create MovieTranslation

---
+++ SkjaTextar/ViewModels/MovieTranslationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SkjaTextar.Models;
+
+namespace SkjaTextar.ViewModels
+{
+    public class MovieTranslationViewModel
+    {
+        public Movie Movie { get; set; }
+        public string Language { get; set; }
+    }
+}
