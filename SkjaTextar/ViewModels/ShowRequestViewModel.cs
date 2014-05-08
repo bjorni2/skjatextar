23f0587bfa5fd8f48017dc472703202f1c1b8081
ingolfurh <satan666>
2014-05-08 10:03:10 +0000

    Bætti við ShowRequestViewModel og lagfærði ShowRequestDetails view'ið

---
+++ SkjaTextar/ViewModels/ShowRequestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SkjaTextar.Models;
+
+namespace SkjaTextar.ViewModels
+{
+	public class ShowRequestViewModel
+	{
+		public Show Show { get; set; }
+
+		public Request Request { get; set; }
+	}
+}
