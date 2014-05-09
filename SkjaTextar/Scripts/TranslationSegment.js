307c888d114bc41b6c2a7ed54144315762857b1f
ingolfurh <satan666>
2014-05-09 12:22:42 +0000

    bættum við javascript á transseg button

---
+++ SkjaTextar/Scripts/TranslationSegment.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".edit").click(function () {
+        // Create a JSON object:
+        var button = $(this).children().first();
+        var input = button.parent().parent().parent().children().first();
+        if(button.hasClass("glyphicon-edit")) {
+            button.removeClass("glyphicon-edit").addClass("glyphicon-floppy-disk");
+            input.removeAttr("readonly");
+        }
+        else {
+            button.removeClass("glyphicon-floppy-disk").addClass("glyphicon-edit");
+            input.attr("readonly", "readonly");
+        }
+        
+    });
+});
