f75a5a85c04d7a3e08aeb8543381d3f9d116236e
bjorni2 <bjornb13@ru.is>
2014-05-14 16:33:38 +0000

    Subtitle parse error

--- SkjaTextar.Tests/Helpers/SubtitleParserTest.cs
+++ SkjaTextar.Tests/Helpers/SubtitleParserTest.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SkjaTextar.Helpers;
+using SkjaTextar.Models;
+using SkjaTextar.Tests.Mocks;
+using SkjaTextar.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using SkjaTextar.Exceptions;
+using System.Web;
+using System.IO;
 
 namespace SkjaTextar.Tests.Helpers
 {
@@ -8,9 +16,37 @@ namespace SkjaTextar.Tests.Helpers
     public class SubtitleParserTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SubtitleParserTests()
         {
+            var directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            
+            // This file contains a segment with invalid timestamp format
+            try
+            {
+                string path = string.Format("{0}\\{1}", directory, "Content\\timestamperror.srt");
+                var translation = SubtitleParser.Parse(path, "srt");
+            }
+            catch(Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(SubtitleParseException));
+            }
 
+            // This file contains 3 segments, the last one has id 9999 and should therefore be cut off
+            string path2 = string.Format("{0}\\{1}", directory, "Content\\cutsOff.srt");
+            var translation2 = SubtitleParser.Parse(path2, "srt");
+            Assert.IsTrue(translation2.TranslationSegments.Count == 2);
+
+
+            // This file contains a segment with invalid id
+            try
+            {
+                string path = string.Format("{0}\\{1}", directory, "Content\\idError.srt");
+                var translation = SubtitleParser.Parse(path, "srt");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(SubtitleParseException));
+            }
         }
     }
 }
